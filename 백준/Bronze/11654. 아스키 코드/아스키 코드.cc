c23a438102886cf7e54ac17d76eaab5770dffde3
HIT18216 <100738514+HIT18216@users.noreply.github.com>
2023-10-11 16:28:22 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	char c;
+	cin >> c;
+	cout << (int)c;
+}
