f80ae958a26f6b83e88a690165b84f73b9c821df
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-01-30 15:51:11 +0900

    [Silver III] Title: N과 M (1), Time: 328 ms, Memory: 33776 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,44 +1,45 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-
+import java.util.StringTokenizer;
+// src 삭제 후 for 문 index 로 대체
+// complete code 에서 printXX 대신 StringBuilder
 public class Main {
 	
-	static int src[], tgt[];
+	static int N,M;
+	static int tgt[];
 	static boolean select[];
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		int N = (int)s.charAt(0) -48 , M = (int)s.charAt(2) -48;
-		src = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
 		tgt = new int[M];
-		select = new boolean[N];
-		for(int i = 1; i<=N; i++) {
-			src[i-1] = i;
-		}
+		select = new boolean[N +1];
+		
 		perm(0);
+		System.out.println(sb);
 	}
 	
 	static void perm(int tgtIdx) {
-		if(tgtIdx == tgt.length) {
-			for(int i = 0; i <tgt.length; i++){
-				System.out.print(tgt[i] + " ");
+		// 기저조건
+		if(tgtIdx == M) {
+			// Complete Code
+			for(int n: tgt) {
+				sb.append(n+" ");
 			}
-			System.out.println();
+			sb.append('\n');
 			return;
 		}
 		
-		for(int i = 0; i < src.length; i++) {
-			if(select[i] ) continue;
-			
-			tgt[tgtIdx] = src[i];
+		for(int i = 1; i <= N; i++) {
+			if(select[i]) continue;
+			tgt[tgtIdx] = i;
 			select[i] = true;
-			perm(tgtIdx +1);
+			perm(tgtIdx + 1);
 			select[i] = false;
 		}
 	}
