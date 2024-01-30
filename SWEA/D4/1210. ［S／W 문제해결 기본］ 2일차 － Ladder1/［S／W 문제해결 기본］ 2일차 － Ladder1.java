deb20da83ed63a6738bbb55da9934d6abc31dfab
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-01-30 17:03:23 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 150 ms, Memory: 28,868 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		int arr[][] = new int[100][100];
+		int dy[] = {0,0,-1}, dx[] = {-1,1,0};
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		for(int t = 1; t<=10; t++) {
+			int y = 99,x = 0;
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i< 100; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j <100; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			for(int i = 0; i< 100; i++) {
+				if(arr[99][i] == 2) {
+					x = i;
+					break;
+				}
+			}
+			while(y != 0) {
+				y--;
+				if((x+dx[0]>=0)&&(x+dx[0]<100)&&(arr[y][x+dx[0]] == 1)) {
+					while((x+dx[0]>=0)&&(x+dx[0]<100)&&(arr[y][x+dx[0]] == 1)) {
+						x = x+dx[0];
+					}
+				}else if((x+dx[1]>=0)&&(x+dx[1]<100)&&(arr[y][x+dx[1]] == 1)) {
+					while((x+dx[1]>=0)&&(x+dx[1]<100)&&(arr[y][x+dx[1]] == 1)) {
+						x = x+dx[1];
+					}
+				}
+			}
+			sb.append("#").append(t).append(" ").append(x).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
