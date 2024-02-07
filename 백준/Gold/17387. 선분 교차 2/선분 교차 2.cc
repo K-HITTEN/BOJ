a6eec166ae07f9c7a1f669cccf058a806c6067c0
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-02-07 16:34:34 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -11,7 +11,15 @@ long long ccw(long long px1, long long py1, long long px2, long long py2, long l
     else return 0;
 }
 
-//bool check(int x1, int x2, )
+bool check(long long x1, long long y1, long long x2, long long y2, long long x3, long long y3){
+    long long i1 = x1, i2 = x2, i3 = x3, j1 = y1, j2 = y2, j3 = y3;
+    if(i1>i2)swap(i1,i2);
+    if(j1>j2)swap(j1,j2);
+    if(i1<=i3&&i3<=i2&&j1<=j3&&j3<=j2){
+                return true;
+    }
+    return false;
+}
 
 int main(void){
     fastio;
@@ -25,35 +33,18 @@ int main(void){
     if(i1*i2 == -1 && j1*j2 == -1){
         cout << 1;
     }else if( i1*i2*j1*j2 == 0){
-        if(i1 == 0){
-            if(px1>px2)swap(px1,px2);
-          
-            if(py1>py2)swap(py1,py2);
-            if(px1<=px3&&px3<=px2&&py1<=py3&&py3<=py2){
-                cout << 1;
-                return 0;
-            }
-        }if(i2== 0){
-            if(px1>px2)swap(px1,px2);
-            if(py1>py2)swap(py1,py2);
-            if(px1<=px4&&px4<=px2&&py1<=py4&&py4<=py2){
-                cout << 1;
-                return 0;
-            }
-        }if(j1 == 0){
-            if(px3>px4)swap(px3,px4);
-            if(py3>py4)swap(py3,py4);
-            if(px3<=px1&&px1<=px4&&py3<=py1&&py1<=py4){
-                cout << 1;
-                return 0;
-            }
-        }if(j2 == 0){
-            if(px3>px4)swap(px3,px4);
-            if(py3>py4)swap(py3,py4);
-            if(px3<=px2&&px2<=px4&&py3<=py2&&py2<=py4){
-                cout << 1;
-                return 0;
-            }
+        if(i1 == 0 && check(px1,py1,px2,py2,px3,py3)){
+            cout << 1;
+            return 0;
+        }if(i2== 0 && check(px1,py1,px2,py2,px4,py4)){
+            cout << 1;
+            return 0;
+        }if(j1 == 0 && check(px3,py3,px4,py4,px1,py1)){
+            cout << 1;
+            return 0;
+        }if(j2 == 0 && check(px3,py3,px4,py4,px2,py2)){
+            cout << 1;
+            return 0;
         }
         cout << 0;
     }else{
