69828d5c8e35cde803b8136d6c1b392661333d03
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-02-13 17:38:01 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int N,M,arr[8],tgt[8];
+bool selects[8];
+
+void perm(int tgtIdx){
+    if(tgtIdx == M){
+        for(int i = 0; i < M; i++){
+            cout << tgt[i] <<" ";
+        }
+        cout <<"\n";
+        return;
+    }
+    for(int i = 0; i<N; i++){
+        if(selects[i]) continue;
+        if(tgtIdx != 0 && arr[i] <=tgt[tgtIdx-1])continue;
+        tgt[tgtIdx] = arr[i];
+        selects[i] = true;
+        perm(tgtIdx+1);
+        selects[i] = false;
+    }
+}
+
+int main(void){
+    fastio;
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+    sort(arr,arr+N);
+    perm(0);
+}
