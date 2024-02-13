d2362b781d2db4a8f64f19ef1fbe64a317ca1622
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-02-13 17:05:48 +0900

    [Silver V] Title: 스네이크버드, Time: 136 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()), L = Integer.parseInt(st.nextToken());
+		int arr[] = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for(int i = 0; i < N; i++) {
+			if(arr[i]<=L) L++;
+			else break;
+		}
+		System.out.println(L);
+	}
+
+}
