d9798c6fc30554969dde248d66b1c9c03dc85add
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-02-13 21:43:32 +0900

    [Silver II] Title: N과 M (11), Time: 220 ms, Memory: 32840 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int N,M,tgt[8];
+vector<int>arr;
+vector<vector<int>> result;
+
+void perm(int tgtIdx){
+    if(tgtIdx == M){
+        vector<int> tmp;
+        for(int i = 0; i < M; i++){
+            tmp.push_back(tgt[i]);
+        }
+        result.push_back(tmp);
+        return;
+    }
+    for(int i = 0; i<arr.size(); i++){
+        tgt[tgtIdx] = arr[i];
+        perm(tgtIdx+1);
+    }
+}
+int main(void){
+    fastio;
+    int tmp;
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        cin >>tmp;
+        arr.push_back(tmp);
+    }
+    sort(arr.begin(),arr.end());
+    arr.erase(unique(arr.begin(),arr.end()),arr.end());
+    perm(0);
+    sort(result.begin(),result.end());
+    result.erase(unique(result.begin(),result.end()),result.end());
+    for(int i = 0; i < result.size(); i++){
+        for(int j = 0; j <result[i].size(); j++){
+            cout << result[i][j] <<" ";
+        }
+        cout <<"\n";
+    }
+}
