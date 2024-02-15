da84373e2b2a9e79d21b1f9931c631d157e0f226
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-02-15 09:33:33 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze II/15829. Hashing/Hashing.cc
+++ 백준/Bronze II/15829. Hashing/Hashing.cc
@@ -7,13 +7,15 @@ using namespace std;
 int main(void){
     fastio;
     int N, tmp;
-    long long sum = 0;
+    long long sum = 0, tmp2;
     char c;
     cin >> N;
     for(int i = 0; i < N; i++){
         cin >> c;
-        tmp = (int)c-96; 
-        sum += (tmp*pow(31,i));
+        tmp = (int)c-96;
+        tmp2 = 1;
+        for(int j = 0; j < i; j++) tmp2=((tmp2%1234567891)*31)%1234567891; 
+        sum += ((tmp%1234567891)*(tmp2%1234567891))%1234567891;
     }
     cout << sum%1234567891;
 }
