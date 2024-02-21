450a7bafa9c8c648f341033cbcab6fdd15e607a8
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-02-21 11:29:13 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#define fastio cin.tie(0) -> sync_with_stdio(0)
+
+using namespace std;
+
+long dp[101] = {0,};
+
+int main(void){
+   fastio;
+   dp[1] = 1;
+   dp[2] = 1;
+   dp[3] = 1;
+   for(int i = 4; i < 101; i++){
+       dp[i] = dp[i-3] + dp[i-2];
+   }
+   int T, n;
+   cin >> T;
+   for(int i = 0; i < T; i++){
+       cin >> n;
+       cout << dp[n] << '\n';
+   }
+}
+
+
