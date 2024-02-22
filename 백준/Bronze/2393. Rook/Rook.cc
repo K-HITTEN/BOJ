7205a1b93657f19f7468128a12e28adcf4785aed
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-02-22 22:35:13 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int
+main (void)
+{
+  fastio;
+cout << "  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/\n    \\_______/\n     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\\n/_______________\\";
+}
