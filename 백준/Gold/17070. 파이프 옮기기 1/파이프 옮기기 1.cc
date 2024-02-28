8934c97166a1e506ca2fc845a8556d89a4ef3aa8
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-02-28 14:45:58 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int map[17][17] = {0,}, result[17][17][3] = {0,};
+
+
+
+int main(void){
+    fastio;
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= N; j++){
+            cin >> map[i][j];
+        }
+    }
+    // 0 가로 1 대각선 2 세로
+    result[1][2][0] = 1;
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= N; j++){
+            if(map[i][j] == 0 && map[i-1][j] == 0 && map[i][j-1] == 0){
+                result[i][j][1] += (result[i-1][j-1][0] + result[i-1][j-1][1] + result[i-1][j-1][2]);
+            }
+            if(map[i][j] == 0) {
+                result[i][j][0] += (result[i][j-1][0] + result[i][j-1][1]);
+                result[i][j][2] += (result[i-1][j][2] + result[i-1][j][1]);
+            }
+        }
+    }
+    cout << (result[N][N][0]+ result[N][N][1] + result[N][N][2]);
+}
