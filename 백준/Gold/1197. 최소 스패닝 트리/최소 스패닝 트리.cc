514b235eca71e64775027c0ebe04fdc6d644b454
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-02-29 00:09:42 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 40 ms, Memory: 4848 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,75 +1,42 @@
 #include <iostream>
 #include <queue>
-#include <vector>
-#include <algorithm>
-#define fastio cin.tie(0) -> ios_base::sync_with_stdio(0)
+#include <tuple>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
 
 using namespace std;
 
-vector<int> MST[10002];
-bool vertex[10001];
-bool test(int idx, int key){
-    return find(MST[idx].begin(),MST[idx].end(), key) != MST[idx].end();
+int arr[100001] = {0,};
+
+int find(int idx){
+    if (arr[idx] == idx) return idx;
+    else return arr[idx] = find(arr[idx]);
+}
+
+void unions(int x, int y){
+    int nx = find(x), ny = find(y);
+    if(nx>ny) arr[nx] = ny;
+    else arr[ny] = nx;
 }
 
 int main(void){
     fastio;
-    int V, E, A, B, C, sum = 0, mstcount = 0;
+    int V,E,tmp1,tmp2,tmp3;
     cin >> V >> E;
-    priority_queue <pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-    
-    for(int i = 0; i <E; i++){
-        cin >> A >> B >> C;
-        pq.push({C,{A,B}});
+    for(int i = 1; i <= V; i++) arr[i] = i;
+    priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>> pq;
+    for(int i = 0; i < E; i++){
+        cin >> tmp1 >> tmp2 >> tmp3;
+        pq.push({tmp3,tmp1,tmp2});
     }
-    while(!pq.empty()){
-        int v1 = pq.top().second.first , v2 = pq.top().second.second, data = pq.top().first;
+    long count = 0, edgecount = 0;
+    while(edgecount<V-1&&!pq.empty()){
+        int v1 = get<1>(pq.top()), v2 = get<2>(pq.top()), edge = get<0>(pq.top());
         pq.pop();
-        if(vertex[v1] && vertex[v2]){
-            // cout << v1 <<" "<< v2 << endl;;
-            int i;
-            int mstnum1, mstnum2;
-            for(i = 0; i < mstcount; i++){
-                if(test(i, v1) && test(i, v2)) break;
-                if(find(MST[i].begin(),MST[i].end(), v1) != MST[i].end()) mstnum1 = i;
-                if(find(MST[i].begin(),MST[i].end(), v2) != MST[i].end()) mstnum2 = i;
-            }
-            if(i < mstcount) continue;
-            else{
-                if(mstnum1 > mstnum2) swap(mstnum1, mstnum2);
-                for(auto x : MST[mstnum2]) MST[mstnum1].push_back(x);
-                MST[mstnum2].clear();
-            }
-        }
-        else if(!vertex[v1] && !vertex[v2]){
-            if(v1 == v2) continue;
-            MST[mstcount].push_back(v1);
-            MST[mstcount].push_back(v2);
-            vertex[v1] = true;
-            vertex[v2] = true;
-            mstcount++;
-        }else if(vertex[v1]){
-            for(int i = 0; i < mstcount; i++){
-                if(find(MST[i].begin(),MST[i].end(), v1) != MST[i].end()) {
-                    MST[i].push_back(v2);
-                    break;
-                }
-            }
-            vertex[v2] = true;
-        }else{
-           for(int i = 0; i < mstcount; i++){
-                if(find(MST[i].begin(),MST[i].end(), v2) != MST[i].end()) {
-                    MST[i].push_back(v1);
-                    break;
-                }
-            } 
-            vertex[v1] = true;
-        }
-        sum += data;
-        if(MST[0].size() == V){
-            break;
+        if(find(v1) != find(v2)){
+            unions(v1,v2);
+            count += edge;
+            edgecount++;
         }
     }
-    cout << sum << endl;
-    //cout << sum;
+    cout << count;
 }
