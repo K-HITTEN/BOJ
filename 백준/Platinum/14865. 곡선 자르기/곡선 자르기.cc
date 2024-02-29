2428aae591b9afa619ac07c275c8aa25d91a8fdf
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-02-29 10:01:45 +0900

    [Platinum V] Title: 곡선 자르기, Time: 0 ms, Memory: 2024 KB, Score: 11 point -BaekjoonHub

---
+++ 백준/Platinum/14865. 곡선 자르기/곡선 자르기.cc
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio
+
+using namespace std;
+
+struct info{
+    int x;
+    int op;
+};
+
+struct mountain{
+    int left;
+    int right;
+};
+
+struct cmp{
+    bool operator()(mountain &a, mountain &b){
+        return a.left>b.left;
+    }
+};
+
+int main(void){
+    int N,sx,sy,ex,ey,nx,ny;
+    cin >> N >> sx >> sy;
+    ex = sx;
+    ey = sy;
+    queue<info> q;
+    priority_queue<mountain,vector<mountain>,cmp> pq;
+    for(int i = 1; i < N; i++){
+        cin >> nx >> ny;
+        if(ey*ny < 0){
+            info tmp;
+            tmp.x = nx;
+            if(ny>0)tmp.op = 1;
+            else tmp.op = -1;
+            q.push(tmp);
+        }
+        ex = nx;
+        ey = ny;
+    }
+    if(sy*ny <0){
+        info tmp;
+        tmp.x = nx;
+        if(ny>0)tmp.op = 1;
+        else tmp.op = -1;
+        q.push(tmp);
+    }
+    if(q.front().op == -1){
+            q.push(q.front());
+            q.pop();
+        }
+    while(!q.empty()){
+        info left = q.front();
+        q.pop();
+        info right = q.front();
+        q.pop();
+        mountain tmp;
+        if(left.x<right.x){
+            tmp.left = left.x;
+            tmp.right = right.x;
+        }else{
+            tmp.left = right.x;
+            tmp.right = left.x;
+        }
+        pq.push(tmp);
+    }
+    int x1 = INT_MIN, x2 = INT_MIN;
+    int case1 = 0, case2 = 0;
+    while(!pq.empty()){
+        mountain tmp = pq.top();
+        pq.pop();
+        if(x1<tmp.left){
+            case1++;
+            x1 = tmp.right;
+        }
+        if(x2<tmp.left){
+            case2++;
+            x2 = tmp.right;
+        }
+        if(x2>tmp.left){
+            x2 = tmp.right;
+        }
+    }
+    cout << case1 << " " << case2;
+}
