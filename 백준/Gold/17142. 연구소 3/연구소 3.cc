0a54a3d9091222c77a4d1d27db169188737d23aa
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-03 13:33:04 +0900

    [Gold III] Title: 연구소 3, Time: 12 ms, Memory: 2172 KB -BaekjoonHub

---
+++ 백준/Gold/17142. 연구소 3/연구소 3.cc
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+struct info{
+  int y,x,t;  
+};
+
+int map[50][50] = {0,};
+int dy[4] = {-1,1,0,0}, dx[4] = {0,0,-1,1};
+int N,K, safezone = 0;
+int min_time = INT_MAX;
+vector<pair<int,int>> wall;
+vector<pair<int,int>> virus;
+vector<pair<int,int>> tgtVirus;
+
+void bfs(){
+    queue<info> q;
+    bool visited[N][N];
+    fill(&visited[0][0],&visited[N][N],false);
+    for(int i = 0; i < wall.size(); i++){
+        visited[wall[i].first][wall[i].second] = true;
+    }
+    for(int i = 0; i < tgtVirus.size(); i++){
+        visited[tgtVirus[i].first][tgtVirus[i].second] = true;
+        info tmp;
+        tmp.y = tgtVirus[i].first;
+        tmp.x = tgtVirus[i].second;
+        tmp.t = 0;
+        q.push(tmp);
+    }
+    int t = 0;
+    int safe = safezone;
+    while(!q.empty() && safe != 0){
+        info tmp = q.front();
+        q.pop();
+        for(int d = 0; d < 4; d++){
+            int ny = tmp.y + dy[d], nx = tmp.x + dx[d];
+            if(ny <0 || nx <0 || ny >= N || nx >= N || visited[ny][nx])continue;
+            if(tmp.t >= min_time)return;
+            t = tmp.t+1;
+            if(map[ny][nx] == 0)safe--;
+            visited[ny][nx] = true;
+            info tmp2;
+            tmp2.y = ny;
+            tmp2.x = nx;
+            tmp2.t = t;
+            q.push(tmp2);
+        }
+    }
+    if(min_time>t && safe == 0) min_time = t;
+}
+
+
+void comb(int srcIdx,int tgtIdx){
+    if(tgtIdx == K){
+        bfs();
+        return;
+    }
+    if(srcIdx == virus.size())return;
+    tgtVirus[tgtIdx] = virus[srcIdx];
+    comb(srcIdx+1,tgtIdx+1);
+    comb(srcIdx+1,tgtIdx);
+}
+
+int main(void){
+    fastio;
+    cin >> N >> K;
+    tgtVirus.resize(K);
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cin >> map[i][j];
+            if(map[i][j] == 1){
+                wall.push_back({i,j});
+                continue;
+            }
+            else if(map[i][j] == 2)virus.push_back({i,j});
+            else safezone++;
+        }
+    }
+    comb(0,0);
+    if(min_time== INT_MAX)cout << -1;
+    else cout << min_time;
+}
