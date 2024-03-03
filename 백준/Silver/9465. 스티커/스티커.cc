b736f5d262197b9b36bd8fbb9abcc6e6c2369fc1
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-04 00:05:29 +0900

    [Silver I] Title: 스티커, Time: 84 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -26,6 +26,6 @@ int main(void){
             dp[i][0] = max(dp[i-1][1],dp[i-2][1]) + arr[i][0];
             dp[i][1] = max(dp[i-1][0],dp[i-2][0]) + arr[i][1];
         }
-        cout << max(dp[N][0],max(dp[N][1],max(dp[N-1][0],dp[N-2][1]))) << "\n";
+        cout << max(dp[N][0],dp[N][1]) << "\n";
     }
 }
