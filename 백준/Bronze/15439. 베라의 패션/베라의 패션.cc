0c6821328c47e72631c21460a71f86e8bb30449c
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-06 08:49:40 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int N;
+    cin >> N;
+    cout << N*(N-1);
+}
