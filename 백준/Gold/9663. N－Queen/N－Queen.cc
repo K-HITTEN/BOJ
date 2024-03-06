6bce0d19faac3c6dbc2fae4910d7a880b743e9ba
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-06 09:26:29 +0900

    [Gold IV] Title: N-Queen, Time: 1780 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,23 +1,14 @@
 #include <iostream>
+#include <cmath>
 #define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
 
 using namespace std;
 
 int N, count = 0;
 bool map[15][15] = {false,};
-
-bool checking(int idx, int i){
-    int y = idx, check1 = i, check2 = i;
-    while(y >= 0){
-        if(map[y][i])return false;
-        if(check1>=0 && map[y][check1])return false;
-        if(check2<N && map[y][check2])return false;
-        y--;
-        check1--;
-        check2++;
-    }
-    return true;
-}
+bool straight[15] = {false,};
+bool left_cross[30] = {false,};
+bool right_cross[30] = {false,};
 
 void search(int idx){
     if(idx == N){
@@ -25,10 +16,14 @@ void search(int idx){
         return;
     }
     for(int i = 0; i < N; i++){
-        if(checking(idx,i)){
-            map[idx][i] = true;
+        if(!straight[i] && !left_cross[idx-i+15] && !right_cross[i+idx]){
+            straight[i] = true;
+            left_cross[idx-i+15] = true;
+            right_cross[i+idx] = true;
             search(idx+1);
-            map[idx][i] = false;
+            straight[i] = false;
+            left_cross[idx-i+15] = false;
+            right_cross[i+idx] = false;
         }
     }
 }
