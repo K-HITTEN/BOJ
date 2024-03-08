391ca65ca8d8171a689f6c66875641ffbb8946d3
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-08 17:38:07 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+bool transform[101], visited[101] = {false,};
+int info[101] = {0,};
+
+int main(void){
+    fastio;
+    int N, M, tmp, tmp2;
+    cin >> N >> M;
+    for(int i = 0; i < N+M; i++){
+        cin >> tmp >> tmp2;
+        info[tmp] = tmp2;
+        transform[tmp] = true;
+    }
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    visited[1] = true;
+    pq.push({0,1});
+    while(!pq.empty()){
+        pair<int,int> tmp = pq.top();
+        if(tmp.second == 100){
+            cout << tmp.first;
+            return 0;
+        }
+        pq.pop();
+        for(int i = 1; i <=6; i++){
+            int go = tmp.second+i;
+            if(go>100||visited[go])continue;
+            visited[go] = true;
+            if(transform[go]){
+                go = info[go];
+                if(visited[go])continue;
+            }
+            pq.push({tmp.first+1,go});
+        }
+    }
+}
