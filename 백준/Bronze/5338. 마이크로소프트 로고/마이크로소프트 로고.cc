fd94344b396f1cf7bde266360714fcded331ccc6
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-09 22:31:56 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    cout << "       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|";
+}
