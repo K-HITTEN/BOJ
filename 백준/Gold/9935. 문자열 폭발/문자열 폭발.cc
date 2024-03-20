e788290cf390e1198b53129af701fe7b0c236273
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-20 10:54:53 +0900

    [Gold IV] Title: 문자열 폭발, Time: 20 ms, Memory: 7000 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -10,17 +10,17 @@ int main(void){
     string s1, s2, tmp;
     cin >> s1 >> s2;
     stack<char> st;
+    reverse(s2.begin(),s2.end());
     for(int i = 0; i < s1.length(); i++){
         st.push(s1[i]);
-        if(s1[i]==s2[s2.length()-1] && st.size()>=s2.length()){
+        if(s1[i]==s2[0] && st.size()>=s2.length()){
             tmp = "";
             for(int j = 0; j < s2.length(); j++){
                 tmp += st.top();
                 st.pop();
             }
-            reverse(tmp.begin(),tmp.end());
             if(tmp != s2){
-                for(int j = 0; j < tmp.length(); j++)st.push(tmp[j]);
+                for(int j = tmp.length()-1; j >= 0; j--)st.push(tmp[j]);
             }
         }
     }
