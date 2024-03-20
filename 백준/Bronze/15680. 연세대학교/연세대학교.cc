2787af90240f0edf104c70a9bfbf1e077fb9f362
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-21 08:36:08 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int tmp;
+    cin >> tmp;
+    if(tmp == 0)cout << "YONSEI";
+    else cout << "Leading the Way to the Future";
+}
