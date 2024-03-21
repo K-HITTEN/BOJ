333f1e2f5ffb4dad87bd7d9ed127a2bf1d21415f
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-22 08:55:13 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.cc
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.cc
@@ -1,14 +1,3 @@
 #include <iostream>
 #define fastio cin.tie(0)->ios_base::sync_with_stdio
-
-using namespace std;
-
-int main(void){
-    fastio;
-    int sum = 0, tmp;
-    while(cin >> tmp){
-        if(tmp == -1)break;
-        sum += tmp;
-    }
-    cout << sum;
-}
+using namespace std;int main(void){fastio;int sum = 0, tmp;while(cin >> tmp){if(tmp == -1)break;sum += tmp;}cout << sum;}
