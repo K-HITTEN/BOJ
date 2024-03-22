4719684d386ff631007ff07c6ae017ff20e35810
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-22 09:00:07 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.cc
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.cc
@@ -1,2 +1,2 @@
 #include <iostream>
-using namespace std;int main(void){cin.tie(0)->ios_base::sync_with_stdio;int sum = 0, tmp;while(cin >> tmp){if(tmp == -1)break;sum += tmp;}cout << sum;}
+using namespace std;int main(){int sum = 0, tmp;while(cin >> tmp){if(tmp == -1)break;sum += tmp;}cout << sum;}
