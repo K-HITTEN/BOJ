f23124de058fbcc395420500921aaeed75f50731
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-24 12:27:43 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27294. 몇개고？/몇개고？.cc
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -1,2 +1,2 @@
 #include <iostream>
-int main(void){int T,S;std::cin>>T>>S;if(T>11&&T<17&&S==0)std::cout<<320;else std::cout<<280;}
+int main(void){int T,S;std::cin>>T>>S;(T>11&&T<17&&S==0)?std::cout<<320:std::cout<<280;}
