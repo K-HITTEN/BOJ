3fc89807c82de93a9fdc1524bf40c38010d6614e
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-26 10:08:15 +0900

    [Gold V] Title: 수 나누기 게임, Time: 40 ms, Memory: 7296 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -3,12 +3,12 @@
 #define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
 
 using namespace std;
+int N, result[1000001] = {0,}, max_value = 0;
+bool visited[1000001] = {false,};
+vector<int> v;
 
 int main(void){
     fastio;
-    int N, result[1000001] = {0,}, max_value = 0;
-    bool visited[1000001] = {false,};
-    vector<int> v;
     cin >> N;
     v.resize(N);
     for(int i = 0; i < N; i++){
