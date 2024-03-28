b17c52a402f5eaf0ab7bcdbdee876cdd5bd559c2
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-28 14:28:32 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.cc
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.cc
@@ -1,4 +1,4 @@
 #include <iostream>
 #include <queue>
 #define A [o][p]
-using namespace std;char B[50][50];bool V[50][50][(1<<6)];int N,M;struct I{int y,x,k,m;};int main(){cin>>N>>M;I t;queue<I> q;for(int o=0;o<N;o++)for(int p=0;p<M;p++){cin>>B A;if(B A=='0')q.push({o,p,0,0});}while(!q.empty()){t=q.front();q.pop();if(B[t.y][t.x]=='1'){cout<<t.m;return 0;}for(int d=0;d<4;d++){int o=t.y+"2011"[d]-49,p=t.x+"1120"[d]-49;if(o<0|p<0|o>=N|p>=M|V A[t.k]|B A=='#'|(64<B A&B A<71&(t.k&(1<<B A-65))==0))continue;V A[t.k] = true;if(96<B A&B A<103)q.push({o,p,(t.k|(1<<B A-97)),t.m+1});else q.push({o,p,t.k,t.m+1});}}cout<<-1;}
+using namespace std;char B[50][50];bool V[50][50][(1<<6)];int N,M,o,p,d;struct I{int y,x,k,m;};int main(){cin>>N>>M;I t;queue<I> q;for(o=0;o<N;o++)for(p=0;p<M;p++){cin>>B A;if(B A=='0')q.push({o,p,0,0});}while(!q.empty()){t=q.front();q.pop();if(B[t.y][t.x]=='1'){cout<<t.m;return 0;}for(d=0;d<4;d++){o=t.y+"2011"[d]-49;p=t.x+"1120"[d]-49;if(o<0|p<0|o>=N|p>=M|V A[t.k]|B A=='#'|(64<B A&B A<71&(t.k&(1<<B A-65))==0))continue;V A[t.k] = true;if(96<B A&B A<103)q.push({o,p,(t.k|(1<<B A-97)),t.m+1});else q.push({o,p,t.k,t.m+1});}}cout<<-1;}
