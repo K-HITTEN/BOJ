5bf177cb2f4cd637a6379c829f7d123ea3f79016
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-03-28 12:24:43 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 7,581 ms, Memory: 17,448 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.cpp
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <algorithm>
 #define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
-
+ 
 using namespace std;
-
+ 
 int map[1001][1001];
-
+ 
 int main(void){
     fastio;
     int T,N,tmp;
     cin >> T;
     for(int t = 1; t <=T; t++){
-        fill(&map[0][0],&map[1000][1001],987654321);
         cin >> N;
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 cin >> tmp;
-                if(tmp == 1)map[i][j] = 1;
                 if(i==j)map[i][j] = 0;
+                else if(tmp == 1)map[i][j] = 1;
+                else map[i][j] = 987654321;
             }
         }
         int min_value = 987654321,sum;
