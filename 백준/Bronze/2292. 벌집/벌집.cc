dc60472a0bdee86e86ceb23ff1240014278ad261
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-04-07 12:06:58 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.cc
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -1,20 +1,15 @@
 #include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
 
 using namespace std;
 
 int main(void) {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int N;
+	fastio;
+	int N, i = 0;
 	cin >> N;
-	int i = 0;	
 	for (int sum = 2; sum <= N; i++) {
 		sum += 6 * i;
 	}
-	if (N == 1) {
-		cout << 1;
-	}
-	else {
-		cout << i;
-	}
+	if (N == 1)cout << 1;
+	else cout << i;
 }
