d25781725b1f3fa5da9691f31bca470a0083faaf
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-04-08 10:37:20 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int N;
+    cin >> N;
+    cout << 2000 <<"\n";
+    for(int i = 0; i < 1000; i++){
+        cout << 1000 << " ";
+    }
+    for(int i = 0; i < 1000; i++){
+        cout << 1 << " ";
+    }
+}
