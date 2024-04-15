fd0f7b80b4fb02948a0af6fe52310cb470c20bfa
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-04-15 15:08:29 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 32 ms, Memory: 4044 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
@@ -5,16 +5,14 @@ using namespace std;
 
 int map[500][500], N, max_move = 0, dp[500][500];
 int dy[4] = {-1,1,0,0}, dx[4] = {0,0,-1,1};
-bool visited[500][500];
 
-int dfs(int y, int x, int idx, int cost){
-    if(visited[y][x])return dp[y][x];
-    visited[y][x] = true;
+int dfs(int y, int x, int cost){
+    if(dp[y][x]>0)return dp[y][x];
     dp[y][x] = 1;
     for(int d = 0; d < 4; d++){
         int ny = y+dy[d], nx = x+dx[d];
         if(ny<0||nx<0||ny>=N||nx>=N||cost>=map[ny][nx])continue;
-        dp[y][x] = max(dp[y][x],dfs(ny,nx,idx+1,map[ny][nx])+1);
+        dp[y][x] = max(dp[y][x],dfs(ny,nx,map[ny][nx])+1);
     }
     return dp[y][x];
 }
@@ -30,7 +28,7 @@ int main(void){
     }
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            if(!visited[i][j])dfs(i,j,1,map[i][j]);
+            if(dp[i][j]==0)dfs(i,j,map[i][j]);
             max_move = max(max_move,dp[i][j]);
         }
     }
