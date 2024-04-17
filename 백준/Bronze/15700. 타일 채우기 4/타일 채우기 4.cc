8ec3c9f36a0b656f884665c8e7b81a76ebf547c5
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-04-18 08:59:41 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    long long N, M;
+    cin >> N >> M;
+    N *=M;
+    cout << N/2;
+}
+
