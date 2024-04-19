ca516abb16973149fb621f8b237f17713475f8bf
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-04-19 13:57:18 +0900

    [Platinum V] Title: 홍삼 게임 (Hard), Time: 16 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Platinum/14705. 홍삼 게임 （Hard）/홍삼 게임 （Hard）.cc
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <queue>
+#include <tuple>
+#include <algorithm>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int N,A,B,Da,Db;
+int turn = 987654321;
+bool Avisited[500001][2], Bvisited[500001][2];
+
+int main(void){
+    fastio;
+    cin >> N >> A >> B >> Da >> Db;
+    queue<tuple<int,int,int>> q;
+    Avisited[A][1] = true;
+    Bvisited[B][1] = true;
+    q.push({1,A,1});
+    q.push({2,B,1});
+    while(!q.empty()){
+        tuple<int,int,int> tmp = q.front();
+        q.pop();
+        if(get<0>(tmp) == 1 && 2*get<2>(tmp)-1>=turn){
+            cout << turn; 
+            return 0;
+        }
+        if(get<0>(tmp) == 2 && 2*get<2>(tmp)>=turn){
+            cout << turn; 
+            return 0;
+        }
+        if(get<0>(tmp) == 1){
+            int position = get<1>(tmp) + Da;
+            if(position > N) position -= N;
+            if(Bvisited[position][get<2>(tmp)%2]){
+                cout << 2*get<2>(tmp)-1;
+                return 0;
+            }
+            if(!Avisited[position][(get<2>(tmp)+1)%2]){
+                Avisited[position][(get<2>(tmp)+1)%2] = true;
+                q.push({1,position,get<2>(tmp)+1});
+            }
+            int position2 = get<1>(tmp) - Da;
+            if(position2 < 1) position2 = N + position2;
+            if(Bvisited[position2][get<2>(tmp)%2]){
+                cout << 2*get<2>(tmp)-1;
+                return 0;
+            }
+            if(Bvisited[position][(get<2>(tmp)+1)%2]){
+                turn = min(turn,2*get<2>(tmp));
+            }
+            if(Bvisited[position2][(get<2>(tmp)+1)%2]){
+                turn = min(turn,2*get<2>(tmp));
+            }
+            if(!Avisited[position2][(get<2>(tmp)+1)%2]){
+                Avisited[position2][(get<2>(tmp)+1)%2] = true;
+                q.push({1,position2,get<2>(tmp)+1});
+            }
+        }else{
+            int position = get<1>(tmp) + Db;
+            if(position > N) position -= N;
+            if(Avisited[position][(get<2>(tmp)+1)%2]){
+                cout << 2*get<2>(tmp);
+                return 0;
+            }
+            if(!Bvisited[position][(get<2>(tmp)+1)%2]){
+                Bvisited[position][(get<2>(tmp)+1)%2] = true;
+                q.push({2,position,get<2>(tmp)+1});
+            }
+            int position2 = get<1>(tmp) - Db;
+            if(position2 < 1) position2 = N + position2;
+            if(Avisited[position2][(get<2>(tmp)+1)%2]){
+                cout << 2*get<2>(tmp);
+                return 0;
+            }
+            if(Avisited[position][get<2>(tmp)%2]){
+                turn = min(turn,2*get<2>(tmp)+1);
+            }
+            if(Avisited[position2][get<2>(tmp)%2]){
+                turn = min(turn,2*get<2>(tmp)+1);
+            }
+            if(!Bvisited[position2][(get<2>(tmp)+1)%2]){
+                Bvisited[position2][(get<2>(tmp)+1)%2] = true;
+                q.push({2,position2,get<2>(tmp)+1});
+            }
+        }
+    }
+    cout << "Evil Galazy";
+}
