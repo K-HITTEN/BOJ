8c01d9d0ee3b85e501b103416c3854d0a4ddb242
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-04-20 13:21:28 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -4,13 +4,11 @@ using namespace std;
 
 long long A, B;
 int result=987654321;
-bool FLAG = false;
 
 void search(long long _A,int  idx){
     if(idx>=result)return;
     if(_A == B){
         result = min(idx+1,result);
-        FLAG = true;
     }
     if(_A>B)return;
     search((_A*10)+1,idx+1);
@@ -20,6 +18,6 @@ void search(long long _A,int  idx){
 int main(void){
     cin >> A >> B;
     search(A,0);
-    if(!FLAG)cout << -1;
+    if(result == 987654321)cout << -1;
     else cout << result;
 }
