3d2dc882e7495543e3ed8136c1f837c04e4adb24
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-04-24 09:53:46 +0900

    [Gold III] Title: 중량제한, Time: 40 ms, Memory: 9160 KB -BaekjoonHub

---
+++ 백준/Gold/1939. 중량제한/중량제한.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0);
+
+using namespace std;
+
+struct info{
+    int _next;
+    int cost;
+};
+
+struct cmp{
+    bool operator()(info&a,info&b){
+        return a.cost<b.cost;
+    }
+};
+
+int N, M, s, e;
+bool visited[100001];
+
+vector<info> v[100001];
+
+int main(void){
+    fastio;
+    int tmp1, tmp2, tmp3;
+    cin >> N >> M;
+    for(int i = 0; i < M; i++){
+        cin >> tmp1 >> tmp2 >> tmp3;
+        v[tmp1].push_back({tmp2,tmp3});
+        v[tmp2].push_back({tmp1,tmp3});
+    }
+    cin >> s >> e;
+    priority_queue<info,vector<info>,cmp> pq;
+    for(int i = 0; i < v[s].size(); i++){
+        pq.push(v[s][i]);
+    }
+    visited[s] = true;
+    while(!pq.empty()){
+        info tmp = pq.top();
+        pq.pop();
+        if(tmp._next == e){
+            cout << tmp.cost;
+            return 0;
+        }
+        if(visited[tmp._next])continue;
+        visited[tmp._next] = true;
+        for(int i = 0; i < v[tmp._next].size(); i++){
+            info temp = v[tmp._next][i];
+            if(visited[temp._next])continue;
+            if(temp.cost<=tmp.cost){
+                pq.push(temp);
+            }else{
+                pq.push({temp._next,tmp.cost});
+            }
+        }
+    }
+    cout << 0;
+}
