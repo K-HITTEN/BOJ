4f3c999a21292ec14b89336db19d5e17d05aa726
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-04-25 09:05:36 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    string f = ":fan:",s;
+    cin >> s;
+    cout << f << f << f << "\n" << f << ":" << s << ":" << f << "\n" << f << f << f ;
+}
