80aca9f45f86f36122776d39bec5740b8ac20d82
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-04-30 10:08:35 +0900

    [Platinum V] Title: 카드 게임, Time: 736 ms, Memory: 33276 KB -BaekjoonHub

--- 백준/Platinum/16566. 카드 게임/카드 게임.cc
+++ 백준/Platinum/16566. 카드 게임/카드 게임.cc
@@ -6,7 +6,18 @@
 using namespace std;
 
 vector<int> v;
-bool visited[4000001];
+int parent[4000001];
+
+int find(int x){
+    if(parent[x] == x)return x;
+    return parent[x] = find(parent[x]);
+}
+
+void merge(int x, int y){
+    int px = find(x), py = find(y);
+    if(px>py)parent[py] = px;
+    else parent[px] = py;
+}
 
 int main(void){
     fastio;
@@ -15,17 +26,11 @@ int main(void){
     v.resize(M);
     for(int i = 0; i < M; i++)cin >> v[i];
     sort(v.begin(),v.end());
+    for(int i = 0; i < M; i++)parent[i] = i;
     for(int i = 0; i < K; i++){
         cin >> tmp;
-        int idx = lower_bound(v.begin(),v.end(),tmp) - v.begin();
-        if(tmp == v[idx])idx++;
-        while(true){
-            if(visited[idx])idx++;
-            else{
-                cout << v[idx] << "\n";
-                visited[idx] = true;
-                break;
-            }
-        }
+        int idx = lower_bound(v.begin(),v.end(),tmp+1) - v.begin();
+        cout << v[find(parent[idx])] <<"\n";
+        merge(find(parent[idx]),find(parent[idx])+1);
     }
 }
