273dd302e3f03c9ed7d1a4c6d9342a31ee07aa06
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-05-02 10:35:55 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 156 ms, Memory: 80332 KB -BaekjoonHub

---
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#define MOD 1000000007
+#define fastio cin.tie(0)->ios::sync_with_stdio(0)
+
+using namespace std;
+
+long long N, M, K, from;
+long long arr[10000000];
+void update(long long loc, long long val) {
+    long long idx = loc + from - 1;
+    arr[idx] = val;
+    for (long long i = idx / 2; i >= 1; i /= 2) {
+        arr[i] = (arr[i*2] * arr[i*2+ 1])%MOD ;
+    }
+}
+long long sum(long long l, long long r, long long idx = 1, long long L = 1, long long R = from) {
+    if (l > R or r < L) return 1;
+    else if (l <= L and r >= R) return arr[idx];
+    long long m = (L + R) / 2;
+    return (sum(l, r, idx * 2, L, m) * sum(l, r, idx * 2 + 1, m + 1, R))%MOD;
+}
+ 
+int main() {
+    fastio;
+    cin >> N >> M >> K;
+    if((N&(N-1))==0)from = pow(2,(int)log2(N));
+    else from =  pow(2,(int)log2(N)+1);
+    fill(arr,arr+10000000,1);
+    for (long long i = from; i < from + N; i++) cin >> arr[i];
+    for (long long i = from-1; i >= 1; --i) arr[i] = (arr[i * 2] * arr[i * 2 + 1])%MOD;
+    for (long long i = 0; i < M+K; i++) {
+        long long a, b, c;
+        cin >> a >> b >> c;
+        if (a == 1) {
+            update(b, c);
+        }
+        else {
+            cout << sum(b, c) << "\n";
+        }
+    }
+}
