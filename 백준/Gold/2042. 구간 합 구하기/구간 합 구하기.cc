d12612c38f1299670526acb910f0f9c96bf33db7
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-05-02 10:32:47 +0900

    [Gold I] Title: 구간 합 구하기, Time: 208 ms, Memory: 80332 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,46 +1,40 @@
 #include <iostream>
-#include <vector>
-#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+#include <cmath>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0)
 
 using namespace std;
 
-vector<long long> v, tree;
-long long N, M, K, tmp1, tmp2, tmp3;
-
-void update(long long idx, long long val){
-    while(idx <= N){
-        tree[idx] += val;
-        idx += (idx & -idx);
+long long N, M, K, from;
+long long arr[10000000];
+void update(long long loc, long long val) {
+    long long idx = loc + from - 1;
+    arr[idx] = val;
+    for (long long i = idx / 2; i >= 1; i /= 2) {
+        arr[i] = (arr[i*2] + arr[i*2+ 1]) ;
     }
 }
-
-long long _sum(long long idx){
-    long long answer = 0;
-    while(idx > 0){
-        answer += tree[idx];
-        idx -= (idx & -idx);
-    }
-    return answer;
+long long sum(long long l, long long r, long long idx = 1, long long L = 1, long long R = from) {
+    if (l > R or r < L) return 0;
+    else if (l <= L and r >= R) return arr[idx];
+    long long m = (L + R) / 2;
+    return (sum(l, r, idx * 2, L, m) + sum(l, r, idx * 2 + 1, m + 1, R));
 }
-
-int main(void){
+ 
+int main() {
     fastio;
     cin >> N >> M >> K;
-    v.resize(N+1);
-    tree.resize(N+1);
-    for(int i = 1; i <= N; i++){
-        cin >> v[i];
-        update(i,v[i]);
-    }
-    for(int i = 0; i < M + K; i++){
-        cin >> tmp1 >> tmp2 >> tmp3;
-        if(tmp1 == 1){
-            long long tmp = tmp3 - v[tmp2];
-            v[tmp2] = tmp3;
-            update(tmp2,tmp);
+    if((N&(N-1))==0)from = pow(2,(int)log2(N));
+    else from =  pow(2,(int)log2(N)+1);
+    for (long long i = from; i < from + N; i++) cin >> arr[i];
+    for (long long i = from-1; i >= 1; --i) arr[i] = (arr[i * 2] + arr[i * 2 + 1]);
+    for (long long i = 0; i < M+K; i++) {
+        long long a, b, c;
+        cin >> a >> b >> c;
+        if (a == 1) {
+            update(b, c);
         }
-        else{
-            cout << _sum(tmp3)-_sum(tmp2-1) <<"\n";
+        else {
+            cout << sum(b, c) << "\n";
         }
     }
 }
