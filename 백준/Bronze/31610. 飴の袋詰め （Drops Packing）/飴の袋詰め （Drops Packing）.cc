27b7af33bf7f51c9812cf61be2a41868f38852b7
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-05-08 19:51:39 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    int A,B,C;
+    cin >> A >> B >> C;
+    cout << A*B+C;
+}
