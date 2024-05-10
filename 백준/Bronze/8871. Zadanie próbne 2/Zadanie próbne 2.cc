1814dc827d829d2a4b01a9c7e9f1a4203cf47508
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-05-10 18:54:33 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int n;
+    cin >> n;
+    cout << (n+1)*2 << " " << (n+1)*3;
+}
