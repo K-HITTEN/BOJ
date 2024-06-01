38d9592eeaf5b0e5f471ed4cff8b26971421702e
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-06-01 13:46:07 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int N, sum = 0, tmp;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> tmp;
+        sum += tmp;
+    }
+    cout << sum;
+}
