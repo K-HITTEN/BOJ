8d1d6d87c9357fe931e11705b079d4ea02691513
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-06-02 10:18:02 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    long long N;
+    cin >> N;
+    for(long long i = 0; i < N; i++){
+        cout << "SciComLove\n";
+    }
+}
