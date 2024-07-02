8d45e35cf3a683166a2959763373794c25cbc2d8
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-07-02 17:33:05 +0900

    [Silver V] Title: The Owl and the Fox, Time: 236 ms, Memory: 24788 KB -BaekjoonHub

--- 백준/Silver/11462. The Owl and the Fox/The Owl and the Fox.java
+++ 백준/Silver/11462. The Owl and the Fox/The Owl and the Fox.java
@@ -1,14 +1,16 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static String s;
     static int tmp;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         while (true){
             s = br.readLine();
             if (s.equals("END")){
+                System.out.print(sb);
                 return;
             }
             tmp = Integer.parseInt(s);
@@ -16,7 +18,7 @@ public class Main {
             while(true){
                 if(tmp%d == 0) d *= 10;
                 else {
-                    System.out.println(tmp - (d/10));
+                    sb.append(tmp - (d/10)).append("\n");
                     break;
                 }
             } 
