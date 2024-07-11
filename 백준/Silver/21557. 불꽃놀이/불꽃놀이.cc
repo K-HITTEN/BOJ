313b1fd6b5c449791bffaaffcb57bc63f9231ece
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-07-11 15:41:12 +0900

    [Silver IV] Title: 불꽃놀이, Time: 24 ms, Memory: 2804 KB, Score: 31 point -BaekjoonHub

---
+++ 백준/Silver/21557. 불꽃놀이/불꽃놀이.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    vector<int> v;
+    int N;
+    cin >> N;
+    v.resize(N);
+    for(int i = 0; i < N; i++)cin >> v[i];
+    for(int i = 0; i < N-2; i++){
+        if(v[0] > v[N-1])v[0]--;
+        else v[N-1]--;
+    }
+    cout << max(v[0],v[N-1]);
+}
