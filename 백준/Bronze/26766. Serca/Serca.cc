f198e304042c1c9267348507d8440cce563ea9fc
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-07-17 08:45:08 +0900

    [Bronze V] Title: Serca, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++)cout<<" @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n";
+}
