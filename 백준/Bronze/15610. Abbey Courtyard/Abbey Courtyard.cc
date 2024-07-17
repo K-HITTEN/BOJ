2344b34808d471a2e095f06cc31ea55e5183be79
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-07-18 08:48:17 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    cout << fixed;
+    cout.precision(7);
+    double N;
+    cin >> N;
+    cout << sqrt(N)*4;
+}
