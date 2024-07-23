4e209a2f073cd34fe21dfa8289d5be7ad7ee7780
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-07-23 13:49:34 +0900

    [Silver IV] Title: 불꽃놀이, Time: 24 ms, Memory: 2020 KB, Score: 31 point -BaekjoonHub

--- 백준/Silver/21557. 불꽃놀이/불꽃놀이.cc
+++ 백준/Silver/21557. 불꽃놀이/불꽃놀이.cc
@@ -1,23 +1,24 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
 
 using namespace std;
 
+int N, A, B, X;
+
+
 int main(void){
     fastio;
-    vector<int> v;
-    int N;
-    cin >> N;
-    v.resize(N);
-    for(int i = 0; i < N; i++)cin >> v[i];
-    for(int i = 0; i < N-2; i++){
-        if(i == N-3){
-            v[0]--;
-            v[N-1]--;
-        }
-        else if(v[0] > v[N-1])v[0]--;
-        else v[N-1]--;
+    
+    cin >> N >> A;
+    for(int i = 2; i < N; i++) cin >> X;
+    cin >> B;
+    if(A < B) swap(A, B);
+    for(int i = 2; i < N; i++){
+        if(A > B) A--;
+        else B--;
     }
-    cout << max(v[0],v[N-1]);
+    
+    cout << A;
+    
 }
