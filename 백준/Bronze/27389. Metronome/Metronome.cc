5d3111b661e66eb5f535539ab44f872db33facdf
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-08-08 08:39:20 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    double N;
+    cin >> N;
+    cout << fixed;
+    cout.precision(2);
+    cout << N/4.0;
+}
