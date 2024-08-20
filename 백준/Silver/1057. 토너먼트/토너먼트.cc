214bf6810fdd9f37d1be6fc3d36743c059b2913a
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-08-20 17:39:31 +0900

    [Silver IV] Title: 토너먼트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.cc
+++ 백준/Silver/1057. 토너먼트/토너먼트.cc
@@ -1,16 +1,2 @@
 #include <iostream>
-#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
-
-using namespace std;
-
-int main(void){
-    fastio;
-    int N, a, b, cnt = 0;
-    cin >> N >> a >> b;
-    while(a!=b){
-        a = (a/2)+(a%2);
-        b = (b/2)+(b%2);
-        cnt++;
-    }
-    cout << cnt;
-}
+int main(){int N,a,b,c=0;std::cin>>N>>a>>b;while(a!=b){a=(a/2)+(a%2);b=(b/2)+(b%2);c++;}std::cout<<c;}
