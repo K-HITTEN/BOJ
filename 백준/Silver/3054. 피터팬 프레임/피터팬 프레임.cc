103edf253f99eb6b77086144aa5b233f77acff02
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-08-20 11:05:40 +0900

    [Silver V] Title: 피터팬 프레임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/3054. 피터팬 프레임/피터팬 프레임.cc
+++ 백준/Silver/3054. 피터팬 프레임/피터팬 프레임.cc
@@ -5,33 +5,34 @@ using namespace std;
 
 string s;
 
-int main(void){
-    fastio;
-    cin >> s;
-    cout << '.';
+void s1(){
     for(int i = 1; i <= s.length(); i++){
         if(i%3==0)cout << ".*..";
         else cout << ".#..";
     }
+}
+
+void s2(){
     cout << "\n.";
     for(int i = 1; i<= s.length(); i++){
         if(i%3==0)cout << "*.*.";
         else cout << "#.#.";
     }
+}
+
+int main(void){
+    fastio;
+    cin >> s;
+    cout << '.';
+    s1();
+    s2();
     cout << "\n#";
     for(int i = 1; i <= s.length(); i++){
         cout << '.' << s[i-1] << '.';
         if(((i+1)%3==0&&i+1<=s.length())||i%3==0)cout <<'*';
         else cout <<'#';
     }
+    s2();
     cout << "\n.";
-    for(int i = 1; i<= s.length(); i++){
-        if(i%3==0)cout << "*.*.";
-        else cout << "#.#.";
-    }
-    cout << "\n.";
-    for(int i = 1; i <= s.length(); i++){
-        if(i%3==0)cout << ".*..";
-        else cout << ".#..";
-    }
+    s1();
 }
