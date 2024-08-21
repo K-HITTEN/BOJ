6ec4a70592bc04ae382925bebb561ed8780c6165
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-08-21 16:16:15 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 56 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int N, K, src = 0;
+    cin >> N >> K;
+    vector<int> v;
+    v.resize(N);
+    for(int i = 0; i < N; i++)cin >> v[i];
+    while(N!=0){
+        N--;
+        int max_val = v[N], idx = 0;
+        for(int i = 0; i < N; i++){
+            if(v[i]>max_val){
+                max_val = v[i];
+                idx = i;
+            }
+        }
+        if(max_val != v[N]){
+            swap(v[idx],v[N]);
+            src++;
+        }
+        if(src == K){
+            cout <<  v[idx] << " " << v[N];
+            return 0;
+        }
+    }
+    cout << -1;
+}
