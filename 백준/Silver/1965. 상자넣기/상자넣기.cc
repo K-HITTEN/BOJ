81bf986aed04cdafaa1cb0ccf62c184810ec1be3
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-08-22 16:16:06 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void) {
+	fastio;
+	int N ,tmp;
+	vector <int> check , v ,num;
+	stack <int> st;
+	cin >> N;
+	int n = 1;
+	for (int i = 0; i < N; i++) {
+		cin >> tmp;
+		v.push_back(tmp);
+		if (check.size() == 0 || check.back()< tmp) {
+			check.push_back(tmp);
+			num.push_back(n);
+			n++;
+		}
+		else if (check.back() >= tmp) {
+		    num.push_back(lower_bound(check.begin(), check.end(), tmp)- check.begin()+1);
+			check[lower_bound(check.begin(), check.end(), tmp)- check.begin()] = tmp;
+		}
+	}
+	n -=1;
+	cout << n <<"\n";
+}
+
