dae4dbef4eb68baa79bf4149ba59d902111a5b4e
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-08-23 09:22:52 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int R1, S;
+    cin >> R1 >> S;
+    cout << S*2-R1;
+}
