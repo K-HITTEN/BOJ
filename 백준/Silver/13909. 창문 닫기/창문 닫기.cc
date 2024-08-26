fc07f3c4b42048f6874a2ddf1325e040ed32ca54
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-08-26 13:59:35 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main() {
+    fastio;
+    int N;
+    cin >> N;
+    cout << (int)sqrt(N);
+}
