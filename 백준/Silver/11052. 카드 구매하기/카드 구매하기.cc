57068c6bf49895023237972192702e241c44ff9a
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-08-26 09:59:33 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int dp[1001], arr[1001];
+
+int main(void){
+    fastio;
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; i++){
+        cin >> arr[i];
+    }
+    dp[1] = arr[1];
+    for(int i = 2; i <= N; i++){
+        dp[i] = arr[i];
+        for(int j = 1; j < i; j++){
+            dp[i] = max(dp[i], dp[i-j]+arr[j]);
+        }
+    }
+    cout << dp[N];
+}
