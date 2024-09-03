8f8926267e91672935e48fde537b8fa0b0050975
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-09-03 09:20:32 +0900

    [Gold V] Title: 캠프 준비, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int N, L, R, X, res = 0;
+vector<int> v;
+
+void backtracking(int idx, int sum, int easy, int difficult){
+    if(idx == N){
+        if(sum == 0||sum<L||sum>R||(difficult-easy)<X)return;
+        res++;
+        return;
+    }
+    backtracking(idx+1,sum+v[idx],min(easy,v[idx]),max(difficult,v[idx]));
+    backtracking(idx+1,sum,easy,difficult);
+}
+
+int main(void){
+    fastio;
+    cin >> N >> L >> R >> X;
+    v.resize(N);
+    for(int i = 0; i < N; i++)cin >> v[i];
+    sort(v.begin(),v.end());
+    backtracking(0,0,100000000,0);
+    cout << res;
+}
