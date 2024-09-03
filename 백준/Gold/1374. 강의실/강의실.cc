56987cbf466a9b8ce0d5d36ae2035cb2acfba7c0
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-09-03 09:35:25 +0900

    [Gold V] Title: 강의실, Time: 44 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.cc
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+struct info{
+    int s,f;
+};
+
+struct cmp1{
+    bool operator()(info &a, info &b){return a.s < b.s;}
+};
+
+struct cmp2{
+    bool operator()(info &a, info &b){return a.f > b.f;}
+};
+
+int main(void){
+    fastio;
+    int N, tmp1, tmp2 , tmp3, res = 0;
+    vector<info> v;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> tmp1 >> tmp2 >> tmp3;
+        v.push_back({tmp2, tmp3});
+    }
+    sort(v.begin(), v.end(), cmp1());
+    priority_queue<info,vector<info>,cmp2>pq;
+    for(int i = 0; i < N; i++){
+       pq.push(v[i]);
+       while(!pq.empty()&&pq.top().f<=v[i].s){
+           pq.pop();
+       }
+       int tmp = pq.size();
+       res = max(res,tmp);
+    }
+    cout << res;
+}
