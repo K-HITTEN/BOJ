03413d48a3a7fdb7f6e4a47f7b4adcbae46a6b4b
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-10-16 08:59:49 +0900

    [Bronze IV] Title: 강당 대관, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/31994. 강당 대관/강당 대관.cc
+++ 백준/Bronze/31994. 강당 대관/강당 대관.cc
@@ -1,3 +1,3 @@
 #include<stdio.h>
 #include<string.h>
-int N,n;char s[50],S[50];int main(void){while(scanf("%s %d",s,&n)>1)if(n>N){N=n;strcpy(S,s);}printf("%s",S);}
+int N,n;char s[50],S[50];int main(){while(scanf("%s %d",s,&n)>1)if(n>N){N=n;strcpy(S,s);}printf("%s",S);}
