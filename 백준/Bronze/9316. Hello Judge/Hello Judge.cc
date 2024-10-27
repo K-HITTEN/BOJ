03a342458bb2d37978ab33381dee2aa6cb6d67c0
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-10-27 17:47:43 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#define fastio cin.tie(0)->ios_base::sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; i++){
+        cout << "Hello World, Judge " << i << "!\n";
+    }
+}
