7ec8bf04938a8c34247be58c9813cc2614cc4908
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-11-11 10:47:37 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.cc
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -1,24 +1,18 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<int> v;
-int N;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin>>N;
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    int N;
+    cin >> N;
     v.resize(N);
-    for(int i=0; i<N; i++)cin>>v[i];
-
+    for(int i = 0; i < N; i++)cin >> v[i];
     if(prev_permutation(v.begin(),v.end())){
-        for(int i=0; i<N; i++){
-            cout<<v[i]<<" ";
-         }
-    }
-    else{
-        cout<<-1<<"\n";
-    }
+        for(int i = 0; i < N; i++)cout << v[i] << " ";
+    }else cout << -1;
+
 }
