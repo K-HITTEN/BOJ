d88ec2da9939947c1c5bec0313e9f3b1f3c80f1d
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-11-29 12:28:24 +0900

    [Gold III] Title: 조삼모사, Time: 156 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Gold/1621. 조삼모사/조삼모사.cc
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<tuple>
+
+using namespace std;  
+
+tuple<long long,int,int> dp[1000001]; // 값, 개수, 추적
+int N, K, C, arr[1000001];
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    cin >> N >> K >> C;
+    for(int i = 1; i <= N; i++)cin >> arr[i];
+    for(int i = 1; i <= N; i++){
+        if(i-K>=0){
+            if(get<0>(dp[i-K])+C == get<0>(dp[i-1])+arr[i]){
+                if(get<1>(dp[i-K])<get<1>(dp[i-1])){
+                    dp[i] = dp[i-K];
+                    get<0>(dp[i]) += C;
+                    get<1>(dp[i])++;
+                    get<2>(dp[i]) = i-K;
+                }else{
+                    dp[i] = dp[i-1];
+                    get<0>(dp[i]) += arr[i];
+                    get<2>(dp[i]) = i-1;
+                }
+            }else if(get<0>(dp[i-K])+C < get<0>(dp[i-1])+arr[i]){
+                dp[i] = dp[i-K];
+                get<0>(dp[i]) += C;
+                get<1>(dp[i])++;
+                get<2>(dp[i]) = i-K;
+            }else{
+                dp[i] = dp[i-1];
+                get<0>(dp[i]) += arr[i];
+                get<2>(dp[i]) = i-1;
+            }
+        }else{
+            dp[i] = dp[i-1];
+            get<0>(dp[i]) += arr[i];
+            get<2>(dp[i]) = i-1;
+        }
+    }
+    cout << get<0>(dp[N])<< "\n" << get<1>(dp[N]) << "\n";
+    int tmp = N;
+    while(tmp!=0){
+        if(get<1>(dp[tmp])!=get<1>(dp[get<2>(dp[tmp])]))cout<<get<2>(dp[tmp])+1<<" ";
+        tmp = get<2>(dp[tmp]);
+    }
+}
