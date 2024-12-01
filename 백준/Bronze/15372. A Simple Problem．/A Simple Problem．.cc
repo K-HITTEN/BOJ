b31750cc6776dfb5fc05b8b2d58524d554f5cab6
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-12-01 11:26:16 +0900

    [Bronze IV] Title: A Simple Problem., Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    long long n,k;
+    cin >> n;
+    while(n--){
+        cin >> k;
+        cout << k*k << "\n";
+    }
+}
