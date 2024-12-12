ec3e6b9352c4107bd14772b48938cab3e59ad8a2
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-12-12 13:38:32 +0900

    [Bronze IV] Title: Atrium, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    cout << fixed;
+    cout.precision(7);
+    double N;
+    cin >> N;
+    cout << sqrt(N)*4;
+}
