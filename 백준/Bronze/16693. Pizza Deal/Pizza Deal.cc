7d5395cf742ce68f4eb983be97d883b75fcc6a09
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-12-13 10:22:41 +0900

    [Bronze IV] Title: Pizza Deal, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/16693. Pizza Deal/Pizza Deal.cc
+++ 백준/Bronze/16693. Pizza Deal/Pizza Deal.cc
@@ -7,6 +7,6 @@ int main(){
     cin.tie(0)->ios_base::sync_with_stdio(0);
     double A1, P1, R1, P2;
     cin >> A1 >> P1 >> R1 >> P2;
-    if(A1/P1>(R1*R1*M_PI)/P2)cout << "Slice of pizza";
-    else cout << "Whole pizza";
+    if(P1/A1>P2/(R1*R1*M_PI))cout << "Whole pizza";
+    else cout << "Slice of pizza";
 }
