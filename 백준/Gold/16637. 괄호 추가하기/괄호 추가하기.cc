3efd330dcd7f1e224d6e226949d28b9b5704f6a4
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2024-12-17 13:11:27 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+
+using namespace std;
+
+struct info{
+    int c;
+    bool visited;
+};
+
+int N;
+long long ret = LLONG_MIN;
+char tmp;
+vector<info> v;
+
+void calc(vector<info> arr){
+    for(int i = 1; i < N; i+=2){
+        if(!arr[i-1].visited&&!arr[i].visited&&!arr[i+1].visited){
+            int t1 = arr[i-1].c, t2 = arr[i+1].c;
+            if(arr[i].c == 43)arr[i-1].c = arr[i+1].c = (arr[i-1].c + arr[i+1].c);
+            else if(arr[i].c == 45)arr[i-1].c = arr[i+1].c = (arr[i-1].c - arr[i+1].c);
+            else arr[i-1].c = arr[i+1].c = (arr[i-1].c * arr[i+1].c);
+            arr[i-1].visited = arr[i].visited = arr[i+1].visited = true;
+            calc(arr);
+            arr[i-1].visited = arr[i].visited = arr[i+1].visited = false;
+            arr[i-1].c = t1;
+            arr[i+1].c = t2;
+        }
+    }
+    long long res = arr[0].c;
+    int i = 1;
+    if(arr[0].visited)i=3;
+    for(i; i < N; i+=2){
+        if(arr[i].c == '+')res += arr[i+1].c;
+        else if(arr[i].c == '-')res -= arr[i+1].c;
+        else res *= arr[i+1].c;
+        if(arr[i+1].visited)i+=2;
+    }
+    ret = max(res,ret);
+}
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> tmp;
+        if(tmp=='+')v.push_back({43,false});
+        else if(tmp=='-')v.push_back({45,false});
+        else if(tmp=='*')v.push_back({42,false});
+        else v.push_back({(int)(tmp-'0'),false});
+    }
+    calc(v);
+    cout << ret;
+}
