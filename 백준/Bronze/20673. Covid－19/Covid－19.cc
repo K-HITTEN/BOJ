2851dbf4a2e5164e7ec1e59b009467b5970df3e0
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-01-04 19:39:27 +0900

    [Bronze IV] Title: Covid-19, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20673. Covid－19/Covid－19.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    int p, q;
+    cin >> p >> q;
+    if(p<=50&&q<=10)cout << "White";
+    else if(q>30)cout << "Red";
+    else cout << "Yellow";
+}
