986ecf2ef6ed1a91f254b40c5ce21270b141351a
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-03-30 00:01:17 +0900

    [Bronze IV] Title: Poziome serca, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++)cout << " @@@   @@@  ";
+    cout << "\n";
+    for(int i = 0; i < N; i++)cout << "@   @ @   @ ";
+    cout << "\n";
+    for(int i = 0; i < N; i++)cout << "@    @    @ ";
+    cout << "\n";
+    for(int i = 0; i < N; i++)cout << "@         @ ";
+    cout << "\n";
+    for(int i = 0; i < N; i++)cout << " @       @  ";
+    cout << "\n";
+    for(int i = 0; i < N; i++)cout << "  @     @   ";
+    cout << "\n";
+    for(int i = 0; i < N; i++)cout << "   @   @    ";
+    cout << "\n";
+    for(int i = 0; i < N; i++)cout << "    @ @     ";
+    cout << "\n";
+    for(int i = 0; i < N; i++)cout << "     @      ";
+}
