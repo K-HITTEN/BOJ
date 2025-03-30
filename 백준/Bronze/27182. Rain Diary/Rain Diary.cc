12b98c1bc13018d5e6251c34ea5ea8390e8c626d
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-03-30 20:52:05 +0900

    [Bronze IV] Title: Rain Diary, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27182. Rain Diary/Rain Diary.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    int n, m;
+    cin >> n >> m;
+    if(n>7)cout << n-7;
+    else cout << m+7;
+}
