37b28d1b6639979543161ef3ba04e494c0e1fdb3
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-04-10 14:48:49 +0900

    [Gold IV] Title: 육각수, Time: 980 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/1229. 육각수/육각수.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int dp[1000001];
+vector<int> v;
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    int N, t = 2;
+    cin >> N;
+    fill(dp,dp+1000001,6);
+    dp[0] = 0;
+    v.push_back(1);
+    while(true){
+        if(v.back()+2*t+2*t-3>1000000)break;
+        else{
+            v.push_back(v.back()+2*t+2*t-3);
+            t++;
+        }
+    }
+    for(int i = 1; i < 1000001; i++){
+        for(int j = 0; j < v.size(); j++){
+            if(i>=v[j]){
+                dp[i] = min(dp[i], dp[i-v[j]]+1);
+            }
+            else break;
+        }
+    }
+    cout << dp[N];
+}
