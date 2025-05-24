2a5da6ddd7ed732d8ab1a98e451b839663f3d959
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-05-24 12:12:32 +0900

    [Silver II] Title: 연속합, Time: 4 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,12 +1,17 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include<iostream>
+
+using namespace std;
+
+int N, arr[100001],dp[100001], ret;
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    cin >> N;
+    for(int i = 0; i < N; i++)cin >> arr[i];
+    dp[0] = ret = arr[0];
+    for(int i = 1; i < N; i++){
+        dp[i] = max(arr[i],arr[i]+dp[i-1]);
+        ret = max(ret, dp[i]);
+    }
+    cout << ret;
+}
