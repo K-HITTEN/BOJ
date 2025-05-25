95b998cdccfeb833431b6ed08925a0288b373e7b
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-05-25 14:33:01 +0900

    [Gold III] Title: 나머지 합, Time: 128 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int N, M;
+long long tmp, sum = 0, cnt = 0;
+vector<long long> v;
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    cin >> N >> M;
+    for(int i = 0; i < M; i++)v.push_back(0);
+    for(int i = 0; i < N; i++){
+        cin >> tmp;
+        sum += tmp;
+        v[sum%M]++;
+        if(!(sum%M))cnt++;
+    }
+    for(int i = 0; i <= M; i++){
+        cnt += v[i]*(v[i]-1)/2;
+    }
+    cout << cnt;
+}
