fdc221f1e28b6e577c36aaa586647f6e51d596b2
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-05-25 20:04:12 +0900

    [Gold III] Title: 나머지 합, Time: 124 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,24 +1,44 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
-using namespace std;
+typedef long long int ll;
 
-int N, M;
-long long tmp, sum = 0, cnt = 0;
-vector<long long> v;
+int main()
+{
+    std::cin.tie(0)->ios_base::sync_with_stdio(0);
+   // 입력값 받기.
+   int N, M;
+   std::cin >> N >> M;
 
-int main(){
-    cin.tie(0)->ios_base::sync_with_stdio(0);
-    cin >> N >> M;
-    for(int i = 0; i < M; i++)v.push_back(0);
-    for(int i = 0; i < N; i++){
-        cin >> tmp;
-        sum += tmp;
-        v[sum%M]++;
-        if(!(sum%M))cnt++;
-    }
-    for(int i = 0; i <= M; i++){
-        cnt += v[i]*(v[i]-1)/2;
-    }
-    cout << cnt;
+   // 누적합 저장하는 벡터.
+   std::vector<ll> prefix(N);
+
+   // 누적합의 나머지 계산.
+   std::vector<ll> remainder(N);
+
+   // 나머지가 될 수 있는 수를 저장하는 배열.
+   std::vector<ll> num(M, -1);
+   num[0] = 0;
+
+
+   std::cin >> prefix[0];
+   remainder[0] = prefix[0] % M;
+
+   for (int i = 1; i < N; ++i)
+   {
+      std::cin >> prefix[i];
+      prefix[i] += prefix[i - 1];
+      remainder[i] = prefix[i] % M;
+   }
+
+   //  나누어 떨어지는 구간의 개수 저장.
+   ll count = 0;
+
+   for (int i = 0; i < N; ++i)
+   {
+      count += ++num[remainder[i]];
+   }
+
+   std::cout << count;
+
 }
