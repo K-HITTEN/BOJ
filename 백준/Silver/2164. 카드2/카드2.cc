b3a4852f6d5bc07d382be72b6e862a6208021965
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-05-27 13:40:47 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -3,21 +3,15 @@
 
 using namespace std;
 
-int main(void){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+queue<int> q;
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
     int N;
     cin >> N;
-    queue<int> q;
-    for(int i = 1; i <= N; i++){
-        q.push(i);
-    }
-    while(q.size() != 1){
+    for(int i = 1; i <= N; i++)q.push(i);
+    while(q.size()>1){
         q.pop();
-        if(q.size() == 1){
-            break;
-        }
         q.push(q.front());
         q.pop();
     }
