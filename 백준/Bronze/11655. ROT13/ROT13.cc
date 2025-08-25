8e14af9ee270f702e210594c2798e8e20c827afe
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-08-25 21:33:59 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -6,7 +6,6 @@ using namespace std;
 int main(){
     cin.tie(0)->ios_base::sync_with_stdio(0);
     string s;
-    char c;
     getline(cin,s);
     for(int i = 0; i < s.length(); i++){
         if(('a'<=s[i] && s[i]<='m')||('A'<=s[i] && s[i]<='M'))s[i]+=13;
