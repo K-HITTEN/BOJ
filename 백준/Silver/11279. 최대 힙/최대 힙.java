f6a753b1711c0a62f6be47c39d1a605577db48d9
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-11-30 23:10:13 +0900

    [Silver II] Title: 최대 힙, Time: 320 ms, Memory: 31068 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -2,21 +2,27 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    static class Info{
+        int n;
+        Info(int n){
+            this.n = n;
+        }
+    }
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		PriorityQueue<Info> pq = new PriorityQueue<>((a,b)->b.n-a.n);
 		int tmp, N = Integer.parseInt(st.nextToken());
 		while(N>0) {
 			st = new StringTokenizer(br.readLine());
 			tmp = Integer.parseInt(st.nextToken());
 			if(tmp == 0) {
 				if(pq.isEmpty())sb.append("0\n");
-				else sb.append(pq.poll()).append("\n");
+				else sb.append(pq.poll().n).append("\n");
 			}
 			else {
-				pq.offer(tmp);
+				pq.offer(new Info(tmp));
 			}
 			N--;
 		}
