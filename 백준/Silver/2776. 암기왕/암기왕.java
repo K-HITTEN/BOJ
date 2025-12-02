fbec233a928e9416f2d281d937044e84ec3f0114
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-12-02 20:21:55 +0900

    [Silver IV] Title: 암기왕, Time: 1404 ms, Memory: 285540 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -6,21 +6,21 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb =new StringBuilder();
-        HashMap<String,String> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         int N, M, T = Integer.parseInt(st.nextToken());
         while(T>0){
-            map.clear();
+            set.clear();
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
             for(int i = 0; i < N; i++){
-                map.put(st.nextToken(),i+"");
+                set.add(Integer.parseInt(st.nextToken()));
             }
             st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
             for(int i = 0; i < M; i++){
-                if(map.containsKey(st.nextToken()))sb.append("1\n");
+                if(set.contains(Integer.parseInt(st.nextToken())))sb.append("1\n");
                 else sb.append("0\n");
             }
             T--;
