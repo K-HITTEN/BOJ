f99d834599a5442a18fbad8a513cb6f9e7c9f48b
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2025-12-25 19:28:56 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    string a, b;
+    cin >> a >> b;
+    if(a==b)cout << 0;
+    else cout << 1550;
+}
