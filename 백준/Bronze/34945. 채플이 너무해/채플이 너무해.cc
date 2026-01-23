bb685913bf40319706102c1a2c6cc7c15bc7fc37
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2026-01-24 01:47:29 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    int N;
+    cin >> N;
+    if(N<6)cout << "Oh My God!";
+    else cout << "Success!";
+}
