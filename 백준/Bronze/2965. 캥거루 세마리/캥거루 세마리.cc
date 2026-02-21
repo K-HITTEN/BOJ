57b4e57d3737dd1fcf66bcd7147e1ceefe4d25e7
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2026-02-21 17:32:18 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int arr[3];
+
+int main(){
+    cin.tie(0)->ios_base::sync_with_stdio(0);
+    cin >> arr[0] >> arr[1] >> arr[2];
+    sort(arr, arr+3);
+    cout << max(arr[2]-arr[1],arr[1]-arr[0])-1;
+}
