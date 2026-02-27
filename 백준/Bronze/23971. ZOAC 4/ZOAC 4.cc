54357b9e51c3438bb5e8164c87d1a20f096535e4
Hyunwoo Kim <100738514+K-HITTEN@users.noreply.github.com>
2026-02-27 21:48:10 +0900

    [Bronze III] Title: ZOAC 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+#define ll long long
+
+using namespace std;
+
+double h, w, n, m;
+
+int main(){
+    cin >> h >> w >> n >> m;
+    cout << (ll)ceil(h / (n+1)) * (ll)ceil(w / (m+1));
+}
